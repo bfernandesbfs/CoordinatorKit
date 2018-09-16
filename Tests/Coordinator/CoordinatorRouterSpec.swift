f19bda2d7b2f4a9e1c68c5ca56ce0ee6353b8365
Bruno Fernandes <bruno.fernandesbfs@gmail.com>
2018-09-16 17:14:48 -0300

    Add file of unit test to router

---
+++ Tests/Coordinator/CoordinatorRouterSpec.swift
@@ -0,0 +1,27 @@
+//
+//  CoordinatorRouterSpec.swift
+//  CoordinatorKit
+//
+//  Created by Bruno Fernandes on 9/16/18.
+//  Copyright © 2018 bfernandesbfs. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import CoordinatorKit
+
+class CoordinatorRouterSpec: QuickSpec {
+
+    override func spec() {
+
+        describe("CoordinatorRouterSpec") {
+
+            beforeEach {
+            }
+
+            it("work") {
+            }
+        }
+    }
+}
+
