76feeaaa622b965938085b55a5998867f38c056c
Bruno Fernandes <bruno.santos@concrete.com.br>
2018-09-15 21:16:43 -0300

    Initial Commit

---
+++ CoordinatorKit.playground/Contents.swift
@@ -0,0 +1,5 @@
+//: Playground - noun: a place where people can play
+
+import CoordinatorKit
+
+var str = "Hello, playground"
---
+++ Sources/Core/CoordinatorKit.swift
@@ -0,0 +1,15 @@
+//
+//  CoordinatorKit.swift
+//  CoordinatorKit
+//
+//  Created by Bruno Fernandes on 02/10/17.
+//  Copyright © 2017 bfernandesbfs. All rights reserved.
+//
+
+import Foundation
+
+class CoordinatorKit {
+    
+    static let name = "CoordinatorKit"
+    
+}
